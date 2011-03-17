90fa39641c489da50809a92aead4aee7503e21ae
Jim Karsten <iiijjjiii@gmail.com>
2011-03-17 19:54:45 -0400

    Mod 11011 - Create input.ellwoodepps.com

--- modules/jsonrpclib/__init__.py
+++ modules/jsonrpclib/__init__.py
@@ -1,6 +1,6 @@
-from jsonrpclib.config import Config
+from applications.shared.modules.jsonrpclib.config import Config
 config = Config.instance()
-from jsonrpclib.history import History
+from applications.shared.modules.jsonrpclib.history import History
 history = History.instance()
-from jsonrpclib.jsonrpc import Server, MultiCall, Fault
-from jsonrpclib.jsonrpc import ProtocolError, loads, dumps
+from applications.shared.modules.jsonrpclib.jsonrpc import Server, MultiCall, \
+        Fault, ProtocolError, loads, dumps
