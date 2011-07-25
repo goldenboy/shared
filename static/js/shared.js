e939b6854b29e08de12a16b95a9196a5074da2e1
Jim Karsten <iiijjjiii@gmail.com>
2011-07-25 11:05:36 -0400

    Mod 11108 - Igeejo: Remove CFC ties from firearm input.

--- static/js/shared.js
+++ static/js/shared.js
@@ -1,11 +1,24 @@
 /* Tab to next input on Enter */
 jQuery(document).ready(function(){
-    jQuery('input').keydown( function(e) {
-            var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
-            if(key == 13) {
-                e.preventDefault();
+    function FormHandler() {
+        var submitted_by_enter = false;
+        this.onKeyDown = function(event) {
+            if (event.keyCode == 13) {
+                submitted_by_enter = true;
                 var inputs = jQuery(this).closest('form').find(':input:visible');
                 inputs.eq(inputs.index(this) + 1).focus();
             }
-    });
+        };
+        this.onSubmit = function(event) {
+            if (submitted_by_enter) {
+                submitted_by_enter = false;
+                return false;
+            }
+            return true;
+        }
+    };
+
+    var _form_handler = new FormHandler();
+    jQuery('form').submit(_form_handler.onSubmit);
+    jQuery('input').keydown(_form_handler.onKeyDown);
 });
